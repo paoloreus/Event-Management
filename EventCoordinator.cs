aeedcbd1e4011fad23e438403d38af021bbf62eb
Paolo <74078018+paoloreus@users.noreply.github.com>
2020-11-08 15:33:27 -0500

    Add files via upload

---
+++ EventCoordinator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event_Management
+{
+    class EventCoordinator
+    {
+        CustomerManager custMan;
+        EventManager eventMan;
+
+        public EventCoordinator(int custIdSeed, int maxCust, int eventIdSeed, int maxEvents)
+        {
+            custMan = new CustomerManager(custIdSeed, maxCust);
+            eventMan = new EventManager(eventIdSeed, maxEvents);
+        }
+
+        //customer related methods
+        public bool addCustomer(string fname, string lname, string phone)
+        {
+            return custMan.addCustomer(fname, lname, phone);
+        }
+
+        public string customerList()
+        {
+            return custMan.getCustomerList();
+        }
+
+        public string getCustomerInfoById(int id)
+        {
+            return custMan.getCustomerInfo(id);
+        }
+        public bool deleteCustomer(int id)
+        {
+            return custMan.deleteCustomer(id);
+        }
+
+        // Event related methods
+        public bool addEvent(string name, string venue, Date eventDate, int maxAttendee)
+        {
+            return eventMan.addEvent(name, venue, eventDate, maxAttendee);
+        }
+
+        public string eventList()
+        {
+            return eventMan.getEventList();
+        }
+
+        public string getEventInfoById(int id)
+        {
+            return eventMan.getEventInfo(id);
+        }
+
+
+    }
+}
