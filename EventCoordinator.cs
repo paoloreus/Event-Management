6d17628c8e6fb7ef96a8d3db5159cbdd0ac1914f
Paolo <74078018+paoloreus@users.noreply.github.com>
2020-11-11 21:02:54 -0500

    Add files via upload

--- EventCoordinator.cs
+++ EventCoordinator.cs
@@ -8,11 +8,34 @@ namespace Event_Management
     {
         CustomerManager custMan;
         EventManager eventMan;
+        Event e;
 
         public EventCoordinator(int custIdSeed, int maxCust, int eventIdSeed, int maxEvents)
         {
             custMan = new CustomerManager(custIdSeed, maxCust);
             eventMan = new EventManager(eventIdSeed, maxEvents);
+        }
+
+        public bool registerID(int cid, int eid) {  //registers a customer to a specific event
+            if (custMan.customerExist(cid) && eventMan.eventExists(eid))
+            {
+                e = eventMan.getEvent(eid);   //e will reference the event object returned here
+               
+                if (e.getNumAttendees() < e.getMaxAttendees())
+                {
+
+                    e.addAttendee(custMan.getCustomer(cid));
+                    Console.WriteLine("RSVP has been successfully made");
+                    Console.WriteLine("Press any key to continue ... ");
+                    return true;
+                }
+            }
+            
+                Console.WriteLine("RSVP Failed");
+                Console.WriteLine("Press any key to continue ... ");
+                //Console.ReadLine();
+                return false;
+            
         }
 
         //customer related methods
@@ -37,7 +60,8 @@ namespace Event_Management
 
         // Event related methods
         public bool addEvent(string name, string venue, Date eventDate, int maxAttendee)
-        {
+        {   
+           
             return eventMan.addEvent(name, venue, eventDate, maxAttendee);
         }
 
