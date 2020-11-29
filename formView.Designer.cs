ad58063f50fe64166db7f63ddfefed277e97f783
Prysinamous <74151231+Prysinamous@users.noreply.github.com>
2020-11-29 17:36:19 -0500

    Add files via upload

---
+++ formView.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace GUIpractice
+{
+    partial class formView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblCust = new System.Windows.Forms.Label();
+            this.lblCust2 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLast = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblCust3 = new System.Windows.Forms.Label();
+            this.lblid = new System.Windows.Forms.Label();
+            this.lblCustID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lblCust
+            // 
+            this.lblCust.AutoSize = true;
+            this.lblCust.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCust.Location = new System.Drawing.Point(36, 61);
+            this.lblCust.Name = "lblCust";
+            this.lblCust.Size = new System.Drawing.Size(155, 34);
+            this.lblCust.TabIndex = 2;
+            this.lblCust.Text = "First Name:";
+            this.lblCust.Visible = false;
+            // 
+            // lblCust2
+            // 
+            this.lblCust2.AutoSize = true;
+            this.lblCust2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCust2.Location = new System.Drawing.Point(39, 104);
+            this.lblCust2.Name = "lblCust2";
+            this.lblCust2.Size = new System.Drawing.Size(152, 34);
+            this.lblCust2.TabIndex = 3;
+            this.lblCust2.Text = "Last Name:";
+            this.lblCust2.Visible = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(259, 61);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(86, 34);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Name";
+            this.lblName.Visible = false;
+            // 
+            // lblLast
+            // 
+            this.lblLast.AutoSize = true;
+            this.lblLast.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLast.Location = new System.Drawing.Point(259, 104);
+            this.lblLast.Name = "lblLast";
+            this.lblLast.Size = new System.Drawing.Size(86, 34);
+            this.lblLast.TabIndex = 5;
+            this.lblLast.Text = "Name";
+            this.lblLast.Visible = false;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.Location = new System.Drawing.Point(259, 149);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(121, 34);
+            this.lblPhone.TabIndex = 6;
+            this.lblPhone.Text = "Number:";
+            this.lblPhone.Visible = false;
+            // 
+            // lblCust3
+            // 
+            this.lblCust3.AutoSize = true;
+            this.lblCust3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCust3.Location = new System.Drawing.Point(12, 149);
+            this.lblCust3.Name = "lblCust3";
+            this.lblCust3.Size = new System.Drawing.Size(204, 34);
+            this.lblCust3.TabIndex = 7;
+            this.lblCust3.Text = "Phone Number:";
+            this.lblCust3.Visible = false;
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblid.Location = new System.Drawing.Point(259, 18);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(46, 34);
+            this.lblid.TabIndex = 8;
+            this.lblid.Text = "ID";
+            this.lblid.Visible = false;
+            // 
+            // lblCustID
+            // 
+            this.lblCustID.AutoSize = true;
+            this.lblCustID.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustID.Location = new System.Drawing.Point(27, 18);
+            this.lblCustID.Name = "lblCustID";
+            this.lblCustID.Size = new System.Drawing.Size(177, 34);
+            this.lblCustID.TabIndex = 9;
+            this.lblCustID.Text = "Customer ID:";
+            this.lblCustID.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 504);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // formView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1372, 661);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblCustID);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.lblCust3);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblLast);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblCust2);
+            this.Controls.Add(this.lblCust);
+            this.Location = new System.Drawing.Point(200, 60);
+            this.Name = "formView";
+            this.Text = "View Customers";
+            this.Load += new System.EventHandler(this.formView_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label lblCust;
+        private System.Windows.Forms.Label lblCust2;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLast;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblCust3;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.Label lblCustID;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ formView.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIpractice
+{
+    public partial class formView : Form
+    {
+        Form1 page;
+        EventCoordinator eCoord;
+        CustomerManager cm;
+        int x = 0;
+        int y = 18;
+        public formView()
+        {
+            InitializeComponent();
+        }
+
+        public formView(Form1 f1, EventCoordinator coordinator)
+        {
+            page = f1;
+            InitializeComponent();
+            Label[] labels = new Label[coordinator.getNumCustomers()];
+            Label[] fields = new Label[coordinator.getNumCustomers()];
+
+            for (int i = 0; i < coordinator.getNumCustomers(); i++)
+            {
+                /* lblid.Text = coordinator.getCustomerList()[i].getId().ToString();
+                 lblName.Text = coordinator.getCustomerList()[i].getFirstName();
+                 lblLast.Text = coordinator.getCustomerList()[i].getLastName();
+                 lblPhone.Text = coordinator.getCustomerList()[i].getPhone(); */
+
+                fields[i] = new Label();
+                fields[i].Text = "Customer ID:";
+                fields[i].Location = new Point((x + 70), y);
+
+
+                labels[i] = new Label();
+                labels[i].Text = coordinator.getCustomerList()[i].getId().ToString();
+                labels[i].Location = new Point((x + 70), y);
+
+                this.Controls.Add(fields[i]);
+                this.Controls.Add(labels[i]);
+
+                fields[i].Show();
+                labels[i].Show();
+
+
+
+
+
+            }
+
+        }
+        private void formView_Load(object sender, EventArgs e)
+        {
+
+            //setting up first name
+            /*Label lblFirstName = new Label();
+            lblFirstName.Text = coordinator.getCustomerList()[i].getFirstName();
+            lblFirstName.Location = new Point(400, 60);
+            lblFirstName.AutoSize = true;
+            this.Controls.Add(lblFirstName);
+
+            //setting up last name
+            Label lblLastName = new Label();
+            lblLastName.Text = coordinator.getCustomerList()[i].getLastName();
+            lblLastName.Location = new Point(400, 100);
+            lblLastName.AutoSize = true;
+            this.Controls.Add(lblLastName);
+
+            //setting up phone
+            Label lblPhoneNum = new Label();
+            lblPhoneNum.Text = coordinator.getCustomerList()[i].getPhone();
+            lblPhoneNum.Location = new Point(400, 140);
+            lblPhoneNum.AutoSize = true;
+            this.Controls.Add(lblPhoneNum); */
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            page.Show();
+        }
+    }
+}
