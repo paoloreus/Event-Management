44d6987ac1e8e590a56349b91ea19b9ecf1cd01b
Jai Kumar <68928897+jai-exe@users.noreply.github.com>
2020-11-30 02:08:04 -0500

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace GUIpractice
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rSVPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewCustomersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewCustomerDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewEventsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewEventDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rSVPForEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewRSVPsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.customerToolStripMenuItem,
+            this.eventToolStripMenuItem,
+            this.rSVPToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(612, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // customerToolStripMenuItem
+            // 
+            this.customerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addCustomerToolStripMenuItem,
+            this.viewCustomersToolStripMenuItem,
+            this.viewCustomerDetailsToolStripMenuItem,
+            this.deleteCustomerToolStripMenuItem});
+            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            this.customerToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.customerToolStripMenuItem.Text = "Customer";
+            // 
+            // eventToolStripMenuItem
+            // 
+            this.eventToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addEventToolStripMenuItem,
+            this.viewEventsToolStripMenuItem,
+            this.viewEventDetailsToolStripMenuItem,
+            this.deleteEventToolStripMenuItem});
+            this.eventToolStripMenuItem.Name = "eventToolStripMenuItem";
+            this.eventToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.eventToolStripMenuItem.Text = "Event";
+            this.eventToolStripMenuItem.Click += new System.EventHandler(this.eventToolStripMenuItem_Click);
+            // 
+            // rSVPToolStripMenuItem
+            // 
+            this.rSVPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rSVPForEventToolStripMenuItem,
+            this.viewRSVPsToolStripMenuItem});
+            this.rSVPToolStripMenuItem.Name = "rSVPToolStripMenuItem";
+            this.rSVPToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.rSVPToolStripMenuItem.Text = "RSVP";
+            // 
+            // addCustomerToolStripMenuItem
+            // 
+            this.addCustomerToolStripMenuItem.Name = "addCustomerToolStripMenuItem";
+            this.addCustomerToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.addCustomerToolStripMenuItem.Text = "Add Customer";
+            // 
+            // viewCustomersToolStripMenuItem
+            // 
+            this.viewCustomersToolStripMenuItem.Name = "viewCustomersToolStripMenuItem";
+            this.viewCustomersToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.viewCustomersToolStripMenuItem.Text = "View Customers";
+            // 
+            // viewCustomerDetailsToolStripMenuItem
+            // 
+            this.viewCustomerDetailsToolStripMenuItem.Name = "viewCustomerDetailsToolStripMenuItem";
+            this.viewCustomerDetailsToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.viewCustomerDetailsToolStripMenuItem.Text = "View Customer Details";
+            // 
+            // deleteCustomerToolStripMenuItem
+            // 
+            this.deleteCustomerToolStripMenuItem.Name = "deleteCustomerToolStripMenuItem";
+            this.deleteCustomerToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.deleteCustomerToolStripMenuItem.Text = "Delete Customer";
+            // 
+            // addEventToolStripMenuItem
+            // 
+            this.addEventToolStripMenuItem.Name = "addEventToolStripMenuItem";
+            this.addEventToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addEventToolStripMenuItem.Text = "Add Event";
+            // 
+            // viewEventsToolStripMenuItem
+            // 
+            this.viewEventsToolStripMenuItem.Name = "viewEventsToolStripMenuItem";
+            this.viewEventsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewEventsToolStripMenuItem.Text = "View Events";
+            // 
+            // viewEventDetailsToolStripMenuItem
+            // 
+            this.viewEventDetailsToolStripMenuItem.Name = "viewEventDetailsToolStripMenuItem";
+            this.viewEventDetailsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewEventDetailsToolStripMenuItem.Text = "View Event Details";
+            // 
+            // deleteEventToolStripMenuItem
+            // 
+            this.deleteEventToolStripMenuItem.Name = "deleteEventToolStripMenuItem";
+            this.deleteEventToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteEventToolStripMenuItem.Text = "Delete Event";
+            // 
+            // rSVPForEventToolStripMenuItem
+            // 
+            this.rSVPForEventToolStripMenuItem.Name = "rSVPForEventToolStripMenuItem";
+            this.rSVPForEventToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rSVPForEventToolStripMenuItem.Text = "RSVP for Event";
+            // 
+            // viewRSVPsToolStripMenuItem
+            // 
+            this.viewRSVPsToolStripMenuItem.Name = "viewRSVPsToolStripMenuItem";
+            this.viewRSVPsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewRSVPsToolStripMenuItem.Text = "View RSVPs";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Customer ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Event ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(327, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(327, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(93, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add RSVP";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Back to Main";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 316);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form3";
+            this.Text = "RSVP for Event";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem customerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addCustomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewCustomersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewCustomerDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteCustomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rSVPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewEventsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewEventDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rSVPForEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewRSVPsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ viewRSVP.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace GUIpractice
+{
+    partial class viewRSVP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eventsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rSVPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addCutsomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewCustomersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewCustomerDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewEventDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rSVPForEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewRSVPsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.customerToolStripMenuItem,
+            this.eventsToolStripMenuItem,
+            this.rSVPToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(482, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // customerToolStripMenuItem
+            // 
+            this.customerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addCutsomerToolStripMenuItem,
+            this.viewCustomersToolStripMenuItem,
+            this.viewCustomerDetailsToolStripMenuItem,
+            this.deleteCustomerToolStripMenuItem});
+            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            this.customerToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.customerToolStripMenuItem.Text = "Customer";
+            // 
+            // eventsToolStripMenuItem
+            // 
+            this.eventsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addEventToolStripMenuItem,
+            this.viewEventToolStripMenuItem,
+            this.viewEventDetailsToolStripMenuItem,
+            this.deleteEventToolStripMenuItem});
+            this.eventsToolStripMenuItem.Name = "eventsToolStripMenuItem";
+            this.eventsToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.eventsToolStripMenuItem.Text = "Events";
+            // 
+            // rSVPToolStripMenuItem
+            // 
+            this.rSVPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rSVPForEventToolStripMenuItem,
+            this.viewRSVPsToolStripMenuItem});
+            this.rSVPToolStripMenuItem.Name = "rSVPToolStripMenuItem";
+            this.rSVPToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.rSVPToolStripMenuItem.Text = "RSVP";
+            // 
+            // addCutsomerToolStripMenuItem
+            // 
+            this.addCutsomerToolStripMenuItem.Name = "addCutsomerToolStripMenuItem";
+            this.addCutsomerToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.addCutsomerToolStripMenuItem.Text = "Add Cutsomer";
+            // 
+            // viewCustomersToolStripMenuItem
+            // 
+            this.viewCustomersToolStripMenuItem.Name = "viewCustomersToolStripMenuItem";
+            this.viewCustomersToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.viewCustomersToolStripMenuItem.Text = "View Customers";
+            // 
+            // viewCustomerDetailsToolStripMenuItem
+            // 
+            this.viewCustomerDetailsToolStripMenuItem.Name = "viewCustomerDetailsToolStripMenuItem";
+            this.viewCustomerDetailsToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.viewCustomerDetailsToolStripMenuItem.Text = "View Customer Details";
+            // 
+            // deleteCustomerToolStripMenuItem
+            // 
+            this.deleteCustomerToolStripMenuItem.Name = "deleteCustomerToolStripMenuItem";
+            this.deleteCustomerToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.deleteCustomerToolStripMenuItem.Text = "Delete Customer";
+            // 
+            // addEventToolStripMenuItem
+            // 
+            this.addEventToolStripMenuItem.Name = "addEventToolStripMenuItem";
+            this.addEventToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addEventToolStripMenuItem.Text = "Add Event";
+            // 
+            // viewEventToolStripMenuItem
+            // 
+            this.viewEventToolStripMenuItem.Name = "viewEventToolStripMenuItem";
+            this.viewEventToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewEventToolStripMenuItem.Text = "View Event";
+            // 
+            // viewEventDetailsToolStripMenuItem
+            // 
+            this.viewEventDetailsToolStripMenuItem.Name = "viewEventDetailsToolStripMenuItem";
+            this.viewEventDetailsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewEventDetailsToolStripMenuItem.Text = "View Event Details";
+            // 
+            // deleteEventToolStripMenuItem
+            // 
+            this.deleteEventToolStripMenuItem.Name = "deleteEventToolStripMenuItem";
+            this.deleteEventToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteEventToolStripMenuItem.Text = "Delete Event";
+            // 
+            // rSVPForEventToolStripMenuItem
+            // 
+            this.rSVPForEventToolStripMenuItem.Name = "rSVPForEventToolStripMenuItem";
+            this.rSVPForEventToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rSVPForEventToolStripMenuItem.Text = "RSVP for Event";
+            // 
+            // viewRSVPsToolStripMenuItem
+            // 
+            this.viewRSVPsToolStripMenuItem.Name = "viewRSVPsToolStripMenuItem";
+            this.viewRSVPsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewRSVPsToolStripMenuItem.Text = "View RSVPs";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Back to Main";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(120, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 119);
+            this.textBox1.TabIndex = 2;
+            // 
+            // viewRSVP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 313);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "viewRSVP";
+            this.Text = "viewRSVP";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem customerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addCutsomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewCustomersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewCustomerDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteCustomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eventsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewEventDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rSVPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rSVPForEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewRSVPsToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
